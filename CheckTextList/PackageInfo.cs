f1e7e9b40e29483f6424b62563a8d495d544d031
nebsdsamwu <sam.s.wu@newegg.com>
2016-08-09 14:47:47 -0700

    update

---
+++ CheckTextList/PackageInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckTextList
+{
+    public class PackageInfo
+    {
+        public string TrackingNumber { get; set; }
+        public string Month { get; set; }
+        public double PackageWeight { get; set; }
+        public double DimensionWeight { get; set; }
+        public double ChargeableWeight { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public int Zone { get; set; }
+        public double LengthAndGrith { get; set; }
+        public double NGTotalCharge { get; set; }
+        public double FreightCharge { get; set; }
+        public double FuelCharge { get; set; }
+
+        public PackageInfo()
+        {}
+
+        public PackageInfo(string trackNo)
+        {
+            this.TrackingNumber = trackNo;
+        }
+
+        public string ToCSVString()
+        {
+            return TrackingNumber + ","
+                   + Month + ","
+                   + PackageWeight + ","
+                   + DimensionWeight + ","
+                   + ChargeableWeight + ","
+                   + Length + ","
+                   + Width + ","
+                   + Height + ","
+                   + Zone + ","
+                   + LengthAndGrith + ","
+                   + NGTotalCharge + ","
+                   + FreightCharge + ","
+                   + FuelCharge;
+        }
+    }
+}
--- CheckTextList/Program.cs
+++ CheckTextList/Program.cs
@@ -10,20 +10,119 @@ namespace CheckTextList
     class Program
     {
         static List<string> inLines = new List<string>();
-        static Dictionary<string, int> lineCounts = new Dictionary<string, int>(); 
+        static Dictionary<string, int> lineCounts = new Dictionary<string, int>();
+
+        static List<PackageInfo> CarrierList = new List<PackageInfo>();
+        static Dictionary<string, PackageInfo> CarrierArchive = new Dictionary<string, PackageInfo>();
+
+        static string inputPathMonth = @"E:\00_works\Carrier-checking\Carrier-pkg-month.csv";
+        static string inputPathOther = @"E:\00_works\Carrier-checking\Carrier-jack-result.csv";
+        static string finalOutputPath = @"E:\00_works\Carrier-checking\result\Carrier-result.csv";
+        static string repeatLogpath = @"E:\00_works\Carrier-checking\repeat\Carrier-repeat-pkg.csv";
 
         static void Main(string[] args)
         {
-            string path = @"E:\00_works\00_RMA\Issues\JetRMA_ReturnAmount_Too_Much\jetrmas.txt";
-            string inputPath = @"E:\text\trackings.txt";
-            string outputPath = @"E:\text\trackingsToSql.txt";
+            ReadCarrierMonthCSV(inputPathMonth);
+            ReadCarrierOtherCSV(inputPathOther);
+            WriteCarrierInfo(finalOutputPath);
+            //WriteFile(outputPath);
 
-            ReadFile(inputPath);
-            WriteFile(outputPath);
             //FindRepeat();
             //CheckCounts();
             Console.ReadKey();
         }
+
+        static void WriteCarrierInfo(string outPath)
+        {
+            using (StreamWriter writer = File.AppendText(outPath)) 
+            {
+                writer.WriteLine(@"Tracking,Month,Package Weight,Dimension Weight,Chargeable Weight,Length,Width,Height,Zone,Length & Grith,NG Total Charge,Freight Charge,Fuel Charge");
+                foreach (KeyValuePair<string, PackageInfo> kvp in CarrierArchive)
+                {
+                    Console.WriteLine(kvp.Key);
+                    PackageInfo p = new PackageInfo();
+                    CarrierArchive.TryGetValue(kvp.Key, out p);
+
+                    Console.WriteLine(p.ToCSVString());
+                    writer.WriteLine(p.ToCSVString());
+                }
+            }
+        }
+
+        static void ReadCarrierOtherCSV(string filepath)
+        {
+            int count = 0;
+            char[] sprtr = { ',' };
+            using (StreamReader reader = new StreamReader(filepath))
+            {
+                string lineIn = reader.ReadLine();
+                while (lineIn != null)
+                {
+                    count += 1;
+                    PackageInfo p = new PackageInfo();
+                    string[] cols = lineIn.Split(sprtr);
+
+                    foreach (string s in cols)
+                    {
+                        string trackingNumStr = cols[0].Trim();
+                        if (CarrierArchive.TryGetValue(trackingNumStr, out p))
+                        {
+                            p.TrackingNumber = trackingNumStr;
+                            p.PackageWeight = double.Parse(cols[1].Trim());
+                            p.DimensionWeight = double.Parse(cols[2].Trim());
+                            p.ChargeableWeight = double.Parse(cols[3].Trim());
+                            p.Length = double.Parse(cols[4].Trim());
+                            p.Width = double.Parse(cols[5].Trim());
+                            p.Height = double.Parse(cols[6].Trim());
+                            p.Zone = int.Parse(cols[7].Trim());
+                            p.LengthAndGrith = double.Parse(cols[8].Trim());
+                            p.NGTotalCharge = double.Parse(cols[9].Trim());
+                            p.FreightCharge = double.Parse(cols[10].Trim());
+                            p.FuelCharge = double.Parse(cols[11].Trim());
+                            CarrierArchive[trackingNumStr] = p;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Unmatched Tracking no: " + p.TrackingNumber);
+                        }
+                    }
+                    lineIn = reader.ReadLine();
+                }
+            }
+            Console.WriteLine("Total Other-Info counts: " + count);
+        }
+
+        static void ReadCarrierMonthCSV(string filepath)
+        {
+            int count = 0;
+            char[] sprtr = { ',' };
+            using (StreamReader reader = new StreamReader(filepath))
+            {
+                string lineIn = reader.ReadLine();
+                while (lineIn != null)
+                {
+                    PackageInfo p = new PackageInfo();
+                    count += 1;
+                    string[] cols = lineIn.Split(sprtr);
+                    foreach(string s in cols)
+                    {
+                        p.TrackingNumber = cols[0].Trim();
+                        p.Month = cols[1].Trim();
+                    }
+                    try
+                    {
+                        CarrierArchive.Add(p.TrackingNumber, p);
+                    }
+                    catch(ArgumentException e)
+                    {
+                        //WriteLineTo(p.TrackingNumber, repeatLogpath);
+                    }
+                    lineIn = reader.ReadLine();
+                }
+            }
+            Console.WriteLine("Total counts: " + count);
+        }
+
 
         static void CheckCounts()
         {
@@ -72,7 +171,15 @@ namespace CheckTextList
             Console.WriteLine("Total counts: " + count);
         }
 
-        static void WriteFile(string filepath)
+        static void WriteLineTo(string line, string outPath)
+        {
+            using (StreamWriter writer = File.AppendText(outPath)) //new StreamWriter(filepath))
+            {
+                writer.WriteLine(line);
+            }
+        }
+
+        static void WriteQuotedLine(string filepath)
         {
             if (filepath.Length == 0)
             {
@@ -80,7 +187,7 @@ namespace CheckTextList
             }
 
             string[] lines = inLines.ToArray();
-            using(StreamWriter writer = new StreamWriter(filepath))
+            using(StreamWriter writer = File.AppendText(filepath)) //new StreamWriter(filepath))
             {
                 for (int i = 0; i < lines.Length; i++)
                 {
